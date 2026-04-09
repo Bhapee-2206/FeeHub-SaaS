e78dd9704c8ba3a6b16212de9a65ee9f0dbc6d56
Bharath <balakrishnanbharath546@gmail.com>
2026-04-09 23:40:41 +0530

    Fix white flash: dark body bg on dashboard, defer anti-flash removal to after loader fades

--- frontend/feehub-loader.js
+++ frontend/feehub-loader.js
@@ -123,11 +123,10 @@
     setTimeout(function () {
       loader.classList.add('loader-hidden');
 
-      const antiFlash = document.getElementById('feehub-anti-flash');
-      if (antiFlash) antiFlash.remove();
-
       setTimeout(function () {
         if (loader.parentNode) loader.parentNode.removeChild(loader);
+        const antiFlash = document.getElementById('feehub-anti-flash');
+        if (antiFlash) antiFlash.remove();
       }, 700);
     }, 400);
   }
